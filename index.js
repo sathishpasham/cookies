448fd0871c8c4015c1d037858bfa00f6bd7a2d67
Pasham Sathish Kumar <sathishkumarpasham1998@gmail.com>
2024-05-01 19:23:00 +0530

    third commit

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const express = require("express");
 const app = express();
 const cookieparser=require("cookie-parser")
-app.use(cookieparser);
+app.use(cookieparser());
+
 //home
 //products
 //clearCookie
 
 //-> home get the request i will add the cookie and share with the res
-app.get("/", function (req, res) {
+
+app.get("/", function (req, res){
+  console.log('hi')
   res.cookie("prevpage", "home", { maxAge: 9 * 1000 * 24, httpOnly: true });
   res.status(200).json({
     message: "Thank you for the visit ",
-  });
+  })
+});
 
   // i will chedk the user visiting for the first time or already visted
+
   app.get("/products", function (req, res) {
-    console.log("cookie",req.cookies)
+    if(req.cookies.prevpage){
+      console.log(req.cookies.prevpage);
     res.status(200).json({
-        message :"thank you for accessing route"
+        message :"thank you for accessing route again"
     })
-  });
+ 
+    }else{
+      res.status(200).json({
+        message:"Thank you for accessing route"
+      })
+    }
+    
 });
 
 //clear your cookie
 
-app.get("/clearCookies", function (req, res) {});
+app.get("/clearCookies", function (req, res) {
+    
+});
 
 app.listen(8080, () => {
   console.log("server started");
