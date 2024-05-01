d4fbaf2e522f8d4bc738672f52ec306282183539
Pasham Sathish Kumar <sathishkumarpasham1998@gmail.com>
2024-05-01 15:44:58 +0530

    cookie add

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ const app=express();
 
 
 //-> home get the request i will add the cookie and share with the res
-app.get("/", function(){
+app.get("/", function(req,res){
+
+    res.cookie("prevpage","home",{maxAge:9*1000*24,httpOnly:true})
+    res.status(200).json({
+        message:"Thank you for the visit "
 
 })
 
 // i will chedk the user visiting for the first time or already visted
-app.get("/products", function(){
+app.get("/products", function(req,res){
+    
+    })
 
 })
 
 //clear your cookie
 
-app.get("/clearCookies", function(){
+app.get("/clearCookies", function(req,res){
 
 })
 
